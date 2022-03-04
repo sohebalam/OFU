f597af2cfefd8700546e4421c2aa24f14bb1c864
soheb alam <sohebalam@hotmail.com>
2022-03-04 22:30:11 +0000

    instuctoraccounts

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -57,7 +57,7 @@ export default function ButtonAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ mr: 2, backgroundColor: "white" }}>
         <Toolbar>
           <IconButton
             size="large"
